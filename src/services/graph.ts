228c1758ab1947a0a10e1790aec4719077e65a70
Vijay Kumar <vijay_kumar@isb.edu>
2024-02-06 21:38:17 +0530

    change how node names are built

--- src/services/graph.ts
+++ src/services/graph.ts
@@ -13,26 +13,45 @@ export const compareNodesByLevel = (a: any, b: any) => {
 }
 
 export const buildLGDGraph = (df: any, hierarchy: Hierarchy) => {
-    const lgdColumns = Object.keys(hierarchy);
+    const lgdColumns = Object.entries(hierarchy)
+        .map(([col, lvl]) => ([col, lvl.id]))
+        .sort(compareNodesByLevel)
+        .map(([c]) => c);
     const uniqueRows = df.groupby(lgdColumns).count().loc({ columns: lgdColumns });
     const records = dfd.toJSON(uniqueRows);
 
     const graph = new DirectedGraph();
+
     for (const record of records) {
         let nodes = [];
-        for (const col of lgdColumns) {
-            const lvl = hierarchy[col]
-            const node = [record[col], lvl["id"]]
+        for (let i = 0; i < lgdColumns.length; i++) {
+            const col = lgdColumns[i];
+            const lvl = hierarchy[col];
+            const title = record[col];
+            const node = Array.from({ length: i + 1 }, (_, i) => i)
+                .reduce((level, i) => {
+                    const col: string = lgdColumns[i];
+                    const lvl: { id: string } = hierarchy[col];
+                    const name: string[] = [record[col], lvl.id];
+                    // @ts-ignore
+                    level.push(...name)
+                    return level
+                }, []);
+            const attrs = {
+                title,
+                level_id: lvl.id,
+                level_name: lvl["name"]
+            };
 
-            nodes.push(node)
-            graph.mergeNode(node)
+            nodes.push(node);
+            graph.mergeNode(node, attrs);
         }
         nodes = nodes.sort(compareNodesByLevel)
         for (let i = 0; i < nodes.length - 1; i++) {
             const parent = nodes[i];
             const child = nodes[i + 1];
 
-            graph.mergeEdge(parent, child)
+            graph.mergeEdge(parent, child);
         }
     }
 
