5ca4c821971afee1e5fce27a530033c696527ab7
Vijay Kumar <vijay_kumar@isb.edu>
2024-02-06 21:37:38 +0530

    explorer's active node

--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -1,18 +1,20 @@
 import { DirectedGraph } from "graphology";
 import { FC, ReactNode } from "react";
 import { getRootNodes } from "../services/graph";
+import { LGDNodeAttributes } from "../types";
 
 interface ExplorerProps {
     graph: DirectedGraph;
+    setActiveNode: (node: string) => void;
 }
 
-export const Explorer: FC<ExplorerProps> = ({ graph }) => {
+export const Explorer: FC<ExplorerProps> = ({ graph, setActiveNode }) => {
     const renderNodes = (node: string): JSX.Element => {
         let neighbors: any = graph.outNeighbors(node);
         neighbors = neighbors.map((neighbor) => renderNodes(neighbor));
 
         return (
-            <ExplorerItem title={node} key={node}>
+            <ExplorerItem onClick={() => setActiveNode(node)} attrs={graph.getNodeAttributes(node)} key={node}>
                 {neighbors}
             </ExplorerItem>
         );
@@ -28,17 +30,23 @@ export const Explorer: FC<ExplorerProps> = ({ graph }) => {
 
 
 interface ExplorerItemProps {
-    title: string;
+    onClick: () => void;
+    attrs: LGDNodeAttributes;
     children?: ReactNode[];
 }
-export const ExplorerItem: FC<ExplorerItemProps> = ({ title, children }) => {
+export const ExplorerItem: FC<ExplorerItemProps> = ({  onClick, attrs, children }) => {
+    const hasMatches = !!attrs?.matches;
+    const matches = attrs?.matches;
+
+    const renderText = `${attrs.title}${hasMatches ? `(${matches.length})` : ""}`
+
     if (children?.length === 0) {
-        return <div>{title}</div>
+        return <div className="ml-4" onClick={onClick}>{renderText}</div>
     }
 
     return (
-        <details>
-            <summary>{title}</summary>
+        <details className="ml-4 border-b-1 border-gray-600">
+            <summary onClick={onClick}>{renderText}</summary>
             {children}
         </details>
     )
