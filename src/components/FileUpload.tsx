6d210eb78c3c2da913f12ede142880cbaa94c749
Vijay Kumar <vijay_kumar@isb.edu>
2024-02-06 21:38:40 +0530

    style file upload input

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -22,7 +22,12 @@ export const FileUpload: FC<FileUploaderProps> = ({ onFileLoad }) => {
     }
     return (
         <div>
-            <input type="file" accept='.csv' onChange={handleChange} />
+            <input
+                className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                id="file_input"
+                type="file"
+                accept=".csv"
+                onChange={handleChange} />
         </div>
     )
 }
