4658dec0a4449c4f9d98608141d44626b981d3c3
Vijay Kumar <vijay_kumar@isb.edu>
2024-02-06 21:35:49 +0530

    delete global context

--- src/context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const GlobalContext = createContext({
-    alpha: 1,
-});
-
-export const useGlobalContext = () => useContext(GlobalContext);
