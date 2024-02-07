206ddb66aa40637284b26c2186e5f902d3621992
Vijay Kumar <vijay_kumar@isb.edu>
2024-02-07 10:58:13 +0530

    get parent id before fetching lgd matches

--- src/services/lgd.ts
+++ src/services/lgd.ts
@@ -2,17 +2,43 @@ import { DirectedGraph } from "graphology";
 import { getMatches } from "../api";
 
 
+const getParentNode = (node: string, graph: DirectedGraph) => {
+    const parentNodes = graph.inNeighbors(node);
+    if (parentNodes.length === 0) {
+        return null;
+    }
+    if (parentNodes.length > 1) {
+        throw Error("Nodes are only allowed to have a single parent");
+    }
+    const parent = parentNodes[0];
+    return parent;
+}
+
+const getParentNodeId = (node: string, graph: DirectedGraph) => {
+    const parent = getParentNode(node, graph);
+    if (parent === null) {
+        return null;
+    }
+    const attrs = graph.getNodeAttributes(parent);
+    if (attrs?.match) {
+        return attrs.match.id;
+    }
+    return null;
+}
+
 export const lgdMapGraph = async (graph: DirectedGraph) => {
-    const promises = graph.mapNodes(async node => {
+    for (const node of graph.nodes()) {
         const attrs = graph.getNodeAttributes(node);
         if (!!attrs.title) {
-            const matches = await getMatches(attrs.title, attrs.level_name);
+            const parent_id = getParentNodeId(node, graph);
+            console.log(node, parent_id)
+            const matches = await getMatches(attrs.title, attrs.level_name, parent_id);
             graph.mergeNodeAttributes(node, {
                 matches,
-            })
+                // Matched if only a single match is found,
+                match: matches.length === 1 ? matches[0] : undefined,
+            });
         }
-
-    })
-    await Promise.all(promises);
+    }
     return graph
 }
