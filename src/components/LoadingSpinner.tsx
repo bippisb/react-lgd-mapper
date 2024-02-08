212e9422065a149cd2ba1bae5e4c4fea64db4732
prernasyan <prernasyan22@gmail.com>
2024-02-08 15:08:44 +0530

    first grid styling

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+export default function LoadingSpinner() {
+  return (
+    <div>
+      <div className="spinner-container">
+        <div className="loading-spinner"></div>
+      </div>
+      <div className="flex justify-center text-sm"> Loading the columns</div>
+    </div>
+  );
+}
--- src/components/SelectLGDCols.tsx
+++ src/components/SelectLGDCols.tsx
@@ -1,27 +1,48 @@
-import { FC } from 'react';
+import React, { FC } from "react";
 
 interface SelectLGDColsProps {
-    columns: string[];
-    selectedCols: string[];
-    onSelectionChange: (selectedCols: string[]) => void;
+  columns: string[];
+  selectedCols: string[];
+  onSelectionChange: (selectedCols: string[]) => void;
 }
 
-const SelectLGDCols: FC<SelectLGDColsProps> = ({ columns, selectedCols, onSelectionChange }) => {
-    return (
-        <div>
-            <h3>LGD Columns</h3>
-            <select
-                multiple
-                value={selectedCols}
-                size={columns.length < 10 ? columns.length : 10}
-                onChange={(e) => onSelectionChange(Array.from(e.target.selectedOptions, option => option.value))}
-            >
-                {columns.map(column => (
-                    <option key={column}>{column}</option>
-                ))}
-            </select>
-        </div>
-    );
+const SelectLGDCols: FC<SelectLGDColsProps> = ({
+  columns,
+  selectedCols,
+  onSelectionChange,
+}) => {
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = e.target;
+    if (checked) {
+      onSelectionChange([...selectedCols, value]);
+    } else {
+      onSelectionChange(selectedCols.filter((col) => col !== value));
+    }
+  };
+
+  return (
+    <div className="mt-2 relative">
+      <h3>LGD Columns</h3>
+      <div className="relative w-80 max-h-60 overflow-auto border-black border-1">
+        {
+          <div className="bg-white p-1">
+            {columns.map((column) => (
+              <label key={column} className="block ml-3">
+                <input
+                  type="checkbox"
+                  className="mr-2 accent-gray-800"
+                  value={column}
+                  checked={selectedCols.includes(column)}
+                  onChange={handleCheckboxChange}
+                />
+                {column}
+              </label>
+            ))}
+          </div>
+        }
+      </div>
+    </div>
+  );
 };
 
 export default SelectLGDCols;
