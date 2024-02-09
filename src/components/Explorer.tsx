9fd4f7c95431b93d6b8fe621dee140c5209c9c1c
prernasyan <prernasyan22@gmail.com>
2024-02-09 15:07:51 +0530

    matches color added

--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -4,50 +4,84 @@ import { getRootNodes } from "../services/graph";
 import { LGDNodeAttributes } from "../types";
 
 interface ExplorerProps {
-    graph: DirectedGraph;
-    setActiveNode: (node: string) => void;
+  graph: DirectedGraph;
+  setActiveNode: (node: string) => void;
 }
 
 export const Explorer: FC<ExplorerProps> = ({ graph, setActiveNode }) => {
-    const renderNodes = (node: string): JSX.Element => {
-        let neighbors: any = graph.outNeighbors(node);
-        neighbors = neighbors.map((neighbor) => renderNodes(neighbor));
+  const renderNodes = (node: string): JSX.Element => {
+    let neighbors: any = graph.outNeighbors(node);
+    neighbors = neighbors.map((neighbor) => renderNodes(neighbor));
 
-        return (
-            <ExplorerItem onClick={() => setActiveNode(node)} attrs={graph.getNodeAttributes(node)} key={node}>
-                {neighbors}
-            </ExplorerItem>
-        );
-    };
-
-    const rootNodes = getRootNodes(graph);
     return (
-        <div>
-            {rootNodes.map(root => renderNodes(root))}
-        </div>
-    )
-}
+      <ExplorerItem
+        onClick={() => setActiveNode(node)}
+        attrs={graph.getNodeAttributes(node)}
+        key={node}
+      >
+        {neighbors}
+      </ExplorerItem>
+    );
+  };
 
+  const rootNodes = getRootNodes(graph);
+  return <div>{rootNodes.map((root) => renderNodes(root))}</div>;
+};
 
 interface ExplorerItemProps {
-    onClick: () => void;
-    attrs: LGDNodeAttributes;
-    children?: ReactNode[];
+  onClick: () => void;
+  attrs: LGDNodeAttributes;
+  children?: ReactNode[];
 }
-export const ExplorerItem: FC<ExplorerItemProps> = ({  onClick, attrs, children }) => {
-    const hasMatches = !!attrs?.matches;
-    const matches = attrs?.matches;
+export const ExplorerItem: FC<ExplorerItemProps> = ({
+  onClick,
+  attrs,
+  children,
+}) => {
+  const hasMatches = !!attrs?.matches;
+  //   const matches = attrs?.matches;
+  const numMatches = hasMatches ? attrs.matches.length : 0;
 
-    const renderText = `${attrs.title}${hasMatches ? `(${matches.length})` : ""}`
-
-    if (children?.length === 0) {
-        return <div className="ml-4" onClick={onClick}>{renderText}</div>
+  const getMatchColor = (numMatches: number): string => {
+    if (numMatches === 0) {
+      return "blue";
+    } else if (numMatches === 1) {
+      return "red";
+    } else {
+      return "green";
     }
+  };
 
+  const renderText = (
+    <span>
+      {attrs.title}
+      {hasMatches && (
+        <span style={{ color: getMatchColor(numMatches) }}>({numMatches})</span>
+      )}
+    </span>
+  );
+
+  //   const renderText = `${attrs.title}${hasMatches ? `(${matches.length})` : ""}`;
+
+  /* const renderText = (
+    <span style={{ color: getTextColor() }}>
+      {attrs.title}
+      {hasMatches && `(${matches.length})`}
+    </span>
+  ); */
+
+  if (children?.length === 0) {
     return (
-        <details className="ml-4 border-b-1 border-gray-600">
-            <summary onClick={onClick}>{renderText}</summary>
-            {children}
-        </details>
-    )
-}
+      <div className="ml-4" onClick={onClick}>
+        {renderText}
+      </div>
+    );
+  }
+
+  return (
+    <details className="ml-4 border-b-1 border-gray-600">
+      <summary onClick={onClick}>{renderText}</summary>
+      {children}
+    </details>
+  );
+};
