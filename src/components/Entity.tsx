22a0262159b1a959b6bda0c8b4fadef8fc7265cb
prernasyan <prernasyan22@gmail.com>
2024-02-13 18:07:12 +0530

    MatchListItem component added

--- src/components/Entity.tsx
+++ src/components/Entity.tsx
@@ -4,6 +4,7 @@ import { ILGDMatch } from "../types";
 import { GetLGDMatchComponent } from "./GetLGDMatch";
 import { getParentNodeId } from "../services/lgd";
 import { MatchesTableView } from "./MatchesTableView";
+import MatchListItem from "./MatchListItem";
 
 interface EntityViewProps {
   setGraph: (g: DirectedGraph) => void;
@@ -17,10 +18,10 @@ export const EntityView: FC<EntityViewProps> = ({ node, graph, setGraph }) => {
 
   const setMatch = (match: ILGDMatch) => {
     graph.mergeNodeAttributes(node, {
-      match
-    })
-    setGraph(graph.copy())
-  }
+      match,
+    });
+    setGraph(graph.copy());
+  };
 
   return (
     <div>
@@ -28,16 +29,24 @@ export const EntityView: FC<EntityViewProps> = ({ node, graph, setGraph }) => {
         {attrs.title} <strong>{attrs.level_name}</strong>
       </h4>
       <h5 className="p-2">Matches</h5>
-      <MatchesTableView matches={attrs.matches} node={node} onSelect={setMatch} />
+      <MatchesTableView
+        matches={attrs.matches}
+        node={node}
+        onSelect={setMatch}
+      />
       <GetLGDMatchComponent
         title={attrs.title}
         level={attrs.level_name}
         parent_id={parent_id}
         onSelect={setMatch}
       />
-      <h5>Final Match</h5>
-      <pre>{JSON.stringify(attrs?.match, null, 2)}</pre>
+      {/* <hr className="border border-b-2 border-gray-700" /> */}
+      <div className="mt-2">
+        <h5 className="ml-2 underline">Final Match</h5>
+        {attrs?.match && <MatchListItem match={attrs.match} />}
+      </div>
+
+      {/* <pre>{JSON.stringify(attrs?.match, null, 2)}</pre> */}
     </div>
   );
 };
-
