d95696d2e6c28abf248544a31d068bf0ffa78b6e
Vijay Kumar <vijay_kumar@isb.edu>
2024-03-07 20:48:56 +0530

    refine LGD column selection

--- src/components/SelectLGDCols.tsx
+++ src/components/SelectLGDCols.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { Tooltip } from "./Tooltip";
 
 interface SelectLGDColsProps {
   columns: string[];
@@ -21,16 +22,20 @@ const SelectLGDCols: FC<SelectLGDColsProps> = ({
   };
 
   return (
-    <div className="mt-2 relative">
-      <h3>LGD Columns</h3>
-      <div className="relative w-80 max-h-60 overflow-auto border-black border-1">
+    <div>
+      <hr className="border-gray-600 border-1 my-1"/>
+      <h3 className="flex gap-1">
+        <span>LGD Columns</span>
+        <Tooltip text="Select columns containing names of geographical places"/>
+      </h3>
+      <div className="max-h-60 overflow-auto border-black border-1 p-2">
         {
           <div className="bg-white p-1">
             {columns.map((column) => (
               <label key={column} className="block ml-3">
                 <input
                   type="checkbox"
-                  className="mr-2 accent-gray-800"
+                  className="mr-2 accent-gray-800 align-middle"
                   value={column}
                   checked={selectedCols.includes(column)}
                   onChange={handleCheckboxChange}
