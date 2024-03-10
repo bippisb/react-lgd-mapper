5c14b8fa93ccec34e7ec0bc4ff4403c9e7783034
Vijay Kumar <vijay_kumar@isb.edu>
2024-03-10 08:31:53 +0530

    add api endpoint for fetching batched matches

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
-import { LevelName } from './types';
+import { LevelName, MatchItem } from './types';
 
 const baseURL = 'http://localhost:8000';
 
@@ -13,14 +13,21 @@ export const getLevels = async () => {
   return res.data.filter((l: any) => l.name !== "india")
 }
 
-export const getMatches = async (name: string, level_name: LevelName, parent_id: number | null = null) => {
+export const getMatches = async (name: string, level_name: LevelName, parent_id: number | null = null, with_parents = false) => {
   const res = await axiosInstance.post("/match/entity", {
     name,
     level: level_name,
-    parent_id
+    parent_id,
+    with_parents,
   });
   return res.data;
 }
+
+export const getBatchedMatches = async (payload: MatchItem[], with_parents = false) => {
+  const res = await axiosInstance.post("/match/entity", payload.map(p => ({ ...p, with_parents })));
+  return res.data;
+}
+
 
 export const addVariation = async (variation: string, entity_id: number) => {
   const res = await axiosInstance.post("/add/variation", {
